10536d8a9ffde2a413b467a0e6dff5f4d726fd8c
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-04-18 23:21:35 +0530

    Adding new Movies intio the list

--- src/components/NewMovie/MovieForm.js
+++ src/components/NewMovie/MovieForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./MovieForm.css";
 
 const MovieForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
@@ -18,23 +19,46 @@ const MovieForm = () => {
   };
 
   const submitHandler = (event) => {
-    const movieData = {
+    event.preventDefault();
+    const NewMovieObj = {
       title: enteredTitle,
       opening: enteredOpening,
       date: new Date(enteredDate),
     };
+    console.log(NewMovieObj);
   };
 
   return (
     <>
-      <form onSubmit={submitHandler}>
-        <label>Title</label>
-        <input type="text" onChange={titleChangeHandler}></input>
-        <label>Opening Text</label>
-        <input type="text" onChange={openingTextChangeHandler}></input>
-        <label>Release Date</label>
-        <input type="date" onChange={releaseDateChangeHandler}></input>
-      </form>
+      <section>
+        <form onSubmit={submitHandler}>
+          <label>
+            <b>Title</b>
+          </label>
+          &nbsp;
+          <br />
+          <input type="text" onChange={titleChangeHandler}></input>
+          &nbsp;
+          <br />
+          <label>
+            <b>Opening Text</b>
+          </label>
+          &nbsp;
+          <br />
+          <input type="text" onChange={openingTextChangeHandler}></input>
+          &nbsp;
+          <br />
+          <label>
+            <b>Release Date</b>
+          </label>
+          &nbsp;
+          <br />
+          <input type="date" onChange={releaseDateChangeHandler}></input>
+          &nbsp;
+          <br />
+          <button type="submit">Add Movie</button>
+        </form>
+      </section>
     </>
   );
 };
